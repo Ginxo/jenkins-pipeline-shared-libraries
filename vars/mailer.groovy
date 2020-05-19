5911ef6e3674f63d9d03130cd3fd23b86f3a363d
Enrique Mingorance Cano <ginxaco@gmail.com>
2020-05-19 09:49:06 +0200

    Update mailer.groovy

--- vars/mailer.groovy
+++ vars/mailer.groovy
@@ -3,7 +3,8 @@ def sendEmailFailure() {
     emailext (
             subject: "Build $branch failed",
             body: "Build $branch failed! For more information see $BUILD_URL",
-            recipientProviders: [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']])
+            recipientProviders: [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']]
+    )
 }
 
 def sendEmail_failedPR(String additionalSubject = null ) {
